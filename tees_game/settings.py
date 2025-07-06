6a47853f440ac195781f7cbde1580c1b580e4c56
Albert Vinicius <107517003+albertvinicius10@users.noreply.github.com>
2025-07-06 10:48:23 -0400

    Update settings.py

--- tees_game/settings.py
+++ tees_game/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t(6_()zq$=06-=#=@q43k5()ku$69=cwfit$nxam+egew#kagf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'teesgame-production.up.railway.app']
 
